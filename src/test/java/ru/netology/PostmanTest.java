cdf118cd3bc25ac31c32547823d9f16b039ce8e8
Valeria Oreshko <oreshko-v@yandex.ru>
2023-07-16 21:35:05 +0300

    Initial commit

---
+++ src/test/java/ru/netology/PostmanTest.java
@@ -0,0 +1,26 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanTest {
+
+    @Test
+    void shouldReturnData() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("hi")
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("hi"))
+        ;
+    }
+
+}
